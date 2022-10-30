78b4ee62daf33747f7ae61eb5edf9bd1a0f16031
oren1996 <orenelbazis@outlook.fr>
2022-10-30 10:29:03 +0200

    oren changes 2

--- HW0.py
+++ HW0.py
@@ -1,15 +1,61 @@
-class Node:
-    def __init__(self, value, next_node=None):
+# Q1
+def twoSum(nums: list[int], target: int):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return [i, j]
 
+
+# Q2
+def maxProfit(prices):
+    profit = []
+    for i in range(len(prices)):
+        for j in range(i + 1, len(prices)):
+            profit.append(prices[j] - prices[i])
+    maxx = max(profit)
+    if maxx > 0:
+        return maxx
+    return 0
+
+
+# Q3.1
+class Node:
+
+    def __init__(self, value, next_node=None):
         self.value = value
         self.next = next_node
 
     def __str__(self):
-
         return str(self.value)
 
 
-def sortedMerge(a, b): # This is the merging part like in normal merge sort.
+def read_file(file_path: str):
+    f = open(file_path, 'r')
+    lst = f.readline()
+    headNode = Node(lst[0])
+    return headNode
+
+
+# Q3.2
+def get_length(head: Node):
+    counter = 1
+    while head.next is not None:
+        counter += 1
+        head = head.next
+    return counter
+
+
+# Q3.3
+class Node:
+    def __init__(self, value, next_node=None):
+        self.value = value
+        self.next = next_node
+
+    def __str__(self):
+        return str(self.value)
+
+
+def sortedMerge(a, b):  # This is the merging part like in normal merge sort.
     result = None
 
     # Base cases
@@ -27,6 +73,7 @@ def sortedMerge(a, b): # This is the merging part like in normal merge sort.
         result.next = sortedMerge(a, b.next)
     return result
 
+
 def getMiddle(head):
     if (head == None):
         return head
@@ -35,11 +82,12 @@ def getMiddle(head):
     fast = head
 
     while (fast.next != None and
-            fast.next.next != None):
+           fast.next.next != None):
         slow = slow.next
         fast = fast.next.next
-            
+
     return slow
+
 
 def sort_in_place(head: Node) -> Node:
     if head == None or head.next == None:
@@ -63,7 +111,6 @@ def sort_in_place(head: Node) -> Node:
     return sortedlist
 
 
-
 if __name__ == '__main__':
     node1 = Node(1)
     node2 = Node(2, node1)
@@ -83,6 +130,3 @@ if __name__ == '__main__':
     while tmp2 is not None:
         print(tmp2.value)
         tmp2 = tmp2.next
-
-
-print('feijb')
